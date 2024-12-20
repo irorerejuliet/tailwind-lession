8a1f54261b7c94aef1ead4c5c01e0cd95c4894f7
irorere juliet <irorerejuliet@gmail.com>
2024-12-20 10:45:11 -0900

    make changes in styling

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,22 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        poppins: ["Poppins", "sans-serif"],
+      },
+      colors: {
+        primary: {
+          DEFAULT: "#1D9BF0",
+        },
+        dark: {
+          300: "#14142B",
+        },
+        gray: {
+          secondary: "#BBBBBB",
+        },
+      },
+    },
   },
   plugins: [],
-}
+};
